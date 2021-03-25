1d5f30047dc5ad7a1cfbe3a28f3526d7fccd6b89
Dmitry <vinogradov.dmitry@gmail.com>
2021-03-25 17:32:44 +0200

    Tasks 1 and 3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const tree = path.split('.');
 
+  return function(obj) {
+    let prop = obj;
+    for (const branch of tree) {
+      prop = prop[branch];
+      if (!prop) {
+        break;
+      }
+    }
+    return prop;
+  };
 }
